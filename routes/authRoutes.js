8202cfb9aba8918103578d6fc3ef38de205316eb
Seemo0 <mouallal@valhko.com>
2023-12-26 10:31:22 +0100

    fix + updates

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const login = require("../controllers/authController.js");
+const authJwt = require("../middleware/secureRoute.js")
 
 const authController = new login();
 
@@ -8,6 +9,8 @@ const router = express.Router();
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
+
+router.use(authJwt);
 
 router.post("/logout", authController.logout);
 
