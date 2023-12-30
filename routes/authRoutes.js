71577164d40952406536d06051bc23a87cd02db9
Seemo0 <mouallal@valhko.com>
2023-12-30 22:30:43 +0100

    add route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const login = require("../controllers/authController.js");
-const authJwt = require("../middleware/secureRoute.js")
+const auth = require("../controllers/authController.js");
+const authJwt = require("../middleware/secureRoute.js");
 
-const authController = new login();
+const authController = new auth();
 
 const router = express.Router();
 
 router.post("/register", authController.register);
 
 router.post("/login", authController.login);
+
+router.post("/verify", authController.verifyCode);
 
 router.use(authJwt);
 
