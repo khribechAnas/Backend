a551005875db957d45365ef9622c4422d6a9908f
guernan-othmane <oguernan@gmail.com>
2024-01-05 17:52:13 +0100

    add fire base

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const verifyRoles = require("../middleware/verifyRoles.js");
+const handleFileUploadss = require("../firebase/firebasemiddleware");
 const ProductController = require("../controllers/ProductController");
 
 const productController = new ProductController();
@@ -8,12 +9,17 @@ const productController = new ProductController();
 router.get("/", productController.getAllProduct);
 router.get("/:productId", productController.getProductById);
 router.get("/choose/:categoryId", productController.chooseProductsByCategory);
-router.post(
-  "/add",
+router.post("/add", verifyRoles(["admin"]), productController.addProduct);
+router.delete(
+  "/delete/:productId",
   verifyRoles(["admin"]),
-  productController.addProduct
+  handleFileUploadss.handleFileUploads,
+  productController.removeProduct
 );
-router.delete("/delete/:productId",verifyRoles(["admin"]), productController.removeProduct);
-router.put("/update/:productId",verifyRoles(["admin", "moderator"]), productController.updateProduct);
+router.put(
+  "/update/:productId",
+  verifyRoles(["admin", "moderator"]),
+  productController.updateProduct
+);
 
 module.exports = router;
